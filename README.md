515a9ec8ab65392b0ec20d3ab4a9f27678909706
sayali2004599 <sayalikamble73736@gmail.com>
2026-09-13 00:36:36 +0530

    tools created

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # LangChain-Multi-Agent-Research-System
+
+
+
+conda create -n langagent python=3.11 -y
+
+conda activate language
+
+pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,4 @@
+from src.tools.tools import web_search, scrape_url
+
+r = web_search.invoke("What is the latest research on using AI for climate change mitigation?")
+print(r)
